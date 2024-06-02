79a10b2a4f26f6acb598d35198df88244503bd9c
seul2kim <145472502+seul2kim@users.noreply.github.com>
2024-06-02 15:04:54 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,28 +1,11 @@
 import java.util.Scanner;
 
-/**
- * 백준 전체 1000 문제 알고리즘 클래스
- *
- * @author RWB
- * @see <a href="https://blog.itcode.dev/posts/2021/05/21/a1000">1000 풀이</a>
- * @since 2021.04.21 Wed 21:38:17
- */
-public class Main
-{
-	/**
-	 * 메인 함수
-	 *
-	 * @param args: [String[]] 매개변수
-	 */
-	public static void main(String[] args)
-	{
-		Scanner scanner = new Scanner(System.in);
-		
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		
-		scanner.close();
-		
-		System.out.println(a + b);
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
 }
