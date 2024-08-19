28d5e554a9588c84913d363c468c3d111ddcdcc2
seul2 <145472502+LuckyBranch@users.noreply.github.com>
2024-08-19 21:06:23 +0900

    [Silver IV] Title: 스택, Time: 468 ms, Memory: 37772 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -13,24 +13,19 @@ public class Main {
             String command = sc.nextLine(); // 명령어 입력
             
             if (command.startsWith("push")) {
-                // push 명령어의 경우
                 int X = Integer.parseInt(command.split(" ")[1]);
                 stack.push(X);
             } else if (command.equals("pop")) {
-                // pop 명령어의 경우
                 if (stack.isEmpty()) {
                     System.out.println(-1);
                 } else {
                     System.out.println(stack.pop());
                 }
             } else if (command.equals("size")) {
-                // size 명령어의 경우
                 System.out.println(stack.size());
             } else if (command.equals("empty")) {
-                // empty 명령어의 경우
                 System.out.println(stack.isEmpty() ? 1 : 0);
             } else if (command.equals("top")) {
-                // top 명령어의 경우
                 if (stack.isEmpty()) {
                     System.out.println(-1);
                 } else {
