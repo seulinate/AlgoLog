d21f86f1aa7a44404b0d011ef6e435048a2455c2
yiseul <145472502+seulinate@users.noreply.github.com>
2025-04-13 23:32:43 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 908 ms, Memory: 162048 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,28 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+    public static int[] solution(int[] cards, int[] queries) {
         Map<Integer, Integer> map = new HashMap<>();
 
+        for (int card : cards) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = map.getOrDefault(queries[i], 0);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] cards = new int[N];
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            cards[i] = Integer.parseInt(st.nextToken());
         }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-
+        int[] queries = new int[M];
         for (int i = 0; i < M; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            sb.append(map.getOrDefault(num, 0)).append(" ");
+            queries[i] = Integer.parseInt(st.nextToken());
         }
+        int[] count = solution(cards, queries);
 
+        StringBuilder sb = new StringBuilder();
+        for (int c : count) sb.append(c).append(" ");
         System.out.println(sb);
     }
 }
