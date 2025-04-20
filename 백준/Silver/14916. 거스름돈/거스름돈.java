911bfec079d2a2bbf4eb470dfd1c0239ae389bbe
yiseul <145472502+seulinate@users.noreply.github.com>
2025-04-20 21:21:17 +0900

    [Silver V] Title: 거스름돈, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (n >= 0) {
+            if (n % 5 == 0) {
+                count += n / 5;
+                System.out.println(count);
+                return;
+            }
+            n -= 2; 
+            count++;
+        }
+        System.out.println(-1); 
+    }
+}
